af4761374d25c6fbe7bf5a971318c95d169e1462
Thanush <115856620+Thanush101@users.noreply.github.com>
2025-07-16 10:37:47 +0530

    update rag and pinecone

--- codes/rag_with_chromadb.py
+++ codes/rag_with_chromadb.py
@@ -12,19 +12,19 @@ from langchain_community.document_loaders import PyPDFLoader
 #     "Healthcare innovation is improving patient outcomes."
 # ]
 
-loader = PyPDFLoader(r"E:/Self Made Projects/Ai-Engineering-Roadmap/lessons/Bishop-Pattern-Recognition-and-Machine-Learning-2006.pdf")
+loader = PyPDFLoader(r"C:\Users\thanushthankachan\Desktop\work\Ai enginner Roadmap\Ai-Engineering-Roadmap\lessons\Bishop-Pattern-Recognition-and-Machine-Learning-2006.pdf")
 documents = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
 split_documents = text_splitter.split_documents(documents)
 
-embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
-embeddings = embedding_model.encode(split_documents)
+embedding_model = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
+# embeddings = embedding_model.encode(split_documents)
 # embeddings_func = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-db = Chroma.from_texts(split_documents, embedding_model)
+db = Chroma.from_documents(split_documents, embedding=embedding_model)
 
-query = "What is the future of technology?"
+query = "What is the future of machine learning?"
 results = db.similarity_search(query, k=2)
 for res in results:
     print(res.page_content)
